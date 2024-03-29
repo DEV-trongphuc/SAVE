d3c18ca3a92a76fe10cc07a4c5ad94beaad403ea
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2024-03-29 15:30:07 +0700

    Update birthday.js

--- birthday.js
+++ birthday.js
@@ -5,7 +5,7 @@
 // Hàm trên appscript ngay chỗ htmlBody các bạn thay cái template email trên stripo.email của mình vô nhe 
 // Mấy cái item[số] các bạn sửa lại cho phù hợp với trang tính nhe
 function doGet() {
-  const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet(); //  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Tên tab sheet"); Nếu trang tính bạn có nhiều tab sheet
   const range = sheet.getDataRange();
   const values = range.getValues()
   values.forEach((item)=> {
